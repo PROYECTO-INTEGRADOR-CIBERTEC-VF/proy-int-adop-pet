18356ba4290b7fd4da7c75bd8c607546b8888989
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-07 17:17:21 -0500

    Inicial: Estructura del proyecto .NET Core

---
+++ ProyAdoPet/ProyAdoPet/Models/Mascota.cs
@@ -0,0 +1,12 @@
+﻿namespace ProyAdoPet.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public int Edad { get; set; }
+        public string? Descripcion { get; set; }
+        public string? Estado { get; set; }
+        public string? FotoMascota { get; set; }
+    }
+}
