959d7209fdec825794ec47d1123fbef2fcb3083c
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-18 11:46:08 -0500

    HU-10: Implementado el controlador de administrador que accede a su panel. close #32

---
+++ ProyAdoPet/ProyAdoPet/Controllers/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyAdoPet.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Dashboard() => View();
+    }
+}
