387d624706c1ada75757bc245ee51491ecda457a
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-18 11:51:44 -0500

    HU-10: Implementado la restriccion que da acceso solo a un administrador

--- ProyAdoPet/ProyAdoPet/Controllers/AdminController.cs
+++ ProyAdoPet/ProyAdoPet/Controllers/AdminController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyAdoPet.Constants;
 
 namespace ProyAdoPet.Controllers
 {
+    //solo es accesible por un usuario administrador
+    [Authorize(Roles = RolesConstantes.Administrador)]
     public class AdminController : Controller
     {
         public IActionResult Dashboard() => View();
