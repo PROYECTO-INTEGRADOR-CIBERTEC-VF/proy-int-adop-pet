59334953a59d221db143da4230d95de7cfc4ff02
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-22 16:20:57 -0500

    Ajustes database

---
+++ ProyAdoPet/ProyAdoPet/Database/procediminetos.sql
@@ -0,0 +1,118 @@
+USE ProyectoAdoPet;
+GO
+
+-- =============================================
+-- PROCEDIMIENTO: Registrar Usuario
+-- =============================================
+CREATE OR ALTER PROCEDURE sp_RegistrarUsuario
+    @Nombre NVARCHAR(100),
+    @Correo NVARCHAR(100),
+    @Clave NVARCHAR(MAX),
+	@IdRol INT,
+    @Registrado BIT OUTPUT -- Para avisar si se logro o si el correo ya existía
+AS
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM Usuario WHERE Correo = @Correo)
+    BEGIN
+        INSERT INTO Usuario (Nombre, Correo, Clave, IdRol) VALUES (@Nombre, @Correo, @Clave, @IdRol)
+        SET @Registrado = 1
+    END
+    ELSE
+    BEGIN
+        SET @Registrado = 0
+    END
+END
+GO
+
+-- =============================================
+-- PROCEDIMIENTO: Validar usuario
+-- =============================================
+CREATE OR ALTER PROCEDURE sp_ValidarUsuario
+    @Correo NVARCHAR(100),
+    @Clave NVARCHAR(MAX)
+AS
+BEGIN
+    SELECT IdUsuario, Nombre, Correo, IdRol 
+    FROM Usuario 
+    WHERE Correo = @Correo AND Clave = @Clave
+END
+GO
+
+CREATE OR ALTER PROCEDURE sp_ListarMascotas
+AS
+BEGIN
+    SELECT Id, Nombre, Edad, Descripcion, EstadoId, FotoMascota 
+    FROM Mascota
+    ORDER BY Id DESC;
+END;
+GO
+
+
+-- =============================================
+-- PROCEDIMIENTO: Listado estados
+-- =============================================
+CREATE OR ALTER PROCEDURE sp_ListarEstados
+AS
+BEGIN
+    SELECT Id, Nombre FROM Estado;
+END
+GO
+
+-- =============================================
+-- PROCEDIMIENTO: Registrar mascota
+-- =============================================
+CREATE OR ALTER PROCEDURE sp_RegistrarMascota
+    @Nombre NVARCHAR(100),
+    @Edad NVARCHAR(20),
+    @Descripcion NVARCHAR(MAX),
+    @EstadoId INT,
+    @FotoMascota NVARCHAR(255)
+AS
+BEGIN
+    INSERT INTO Mascota (Nombre, Edad, Descripcion, EstadoId, FotoMascota)
+    VALUES (@Nombre, @Edad, @Descripcion, @EstadoId, @FotoMascota);
+END;
+GO
+
+-- =============================================
+-- PROCEDIMIENTO: Actualizar mascota
+-- =============================================
+CREATE PROCEDURE sp_ActualizarMascota
+    @Id INT,
+    @Nombre VARCHAR(100),
+    @Edad VARCHAR(50),
+    @Descripcion VARCHAR(200),
+    @Estado INT,
+    @FotoMascota VARCHAR(200)
+AS
+BEGIN
+    UPDATE Mascota
+    SET 
+        Nombre = @Nombre,
+        Edad = @Edad,
+        Descripcion = @Descripcion,
+        EstadoId = @Estado,
+        FotoMascota = @FotoMascota
+    WHERE Id = @Id
+END
+
+
+-- =============================================
+-- PROCEDIMIENTO: Obtener mascota por id
+-- =============================================
+CREATE PROCEDURE sp_ObtenerMascota
+    @Id INT
+AS
+BEGIN
+    SELECT 
+        Id,
+        Nombre,
+        Edad,
+        Descripcion,
+        EstadoId,
+        FotoMascota
+    FROM Mascota
+    WHERE Id = @Id
+END
+
+
---
+++ ProyAdoPet/ProyAdoPet/Database/tablas.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE ProyectoAdoPet;
+GO
+
+USE ProyectoAdoPet;
+GO
+
+-- =============================================
+-- TABLA: Roles			
+-- =============================================
+CREATE TABLE Rol (
+    IdRol INT PRIMARY KEY IDENTITY(1,1),
+    NombreRol NVARCHAR(50) NOT NULL
+);
+
+INSERT INTO Rol (NombreRol) VALUES ('Administrador'), ('Usuario');
+GO
+
+-- =============================================
+-- TABLA: User/admin
+-- =============================================
+CREATE TABLE Usuario (
+    IdUsuario INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) NOT NULL,
+    Correo NVARCHAR(100) NOT NULL UNIQUE,
+    Clave NVARCHAR(MAX) NOT NULL,    
+    FechaRegistro DATETIME DEFAULT GETDATE(),
+    IdRol INT DEFAULT 2,
+
+    CONSTRAINT FK_Usuario_Rol
+        FOREIGN KEY (IdRol) REFERENCES Rol(IdRol)
+);
+GO
+
+-- =============================================
+-- TABLA:Estado (mascosta)
+-- =============================================
+CREATE TABLE Estado (
+    Id INT PRIMARY KEY,
+    Nombre NVARCHAR(20) NOT NULL
+);
+GO
+
+--INSERT PRUEBA
+INSERT INTO Estado (Id, Nombre) VALUES (1, 'Disponible');
+INSERT INTO Estado (Id, Nombre) VALUES (2, 'Adoptado');
+GO
+
+-- =============================================
+-- TABLA: Mascota
+-- =============================================
+CREATE TABLE Mascota (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Nombre NVARCHAR(100) NOT NULL,
+    Edad NVARCHAR(20),
+    Descripcion NVARCHAR(MAX),
+    EstadoId INT DEFAULT 1,
+    FotoMascota NVARCHAR(255) NULL,
+    
+    CONSTRAINT FK_Mascota_Estado 
+        FOREIGN KEY (EstadoId) REFERENCES Estado(Id)
+);
+GO
+
+--INSERT PRUEBA
+INSERT INTO Mascota (Nombre, Edad, Descripcion, EstadoId, FotoMascota)
+VALUES ('Max', '3 años', 'Tranquilo y cariñoso, ideal para familia', 1, '0b6856_437e49e1656142bcadfba49e7aa27058~mv2.jpg');
+GO
