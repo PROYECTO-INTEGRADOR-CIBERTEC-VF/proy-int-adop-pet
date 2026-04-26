1ea5981e4f9eab3c98db4a44554d937674efea8f
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-26 17:27:46 -0500

    HU-16: Se implemento el mascotaservive y el metodo get del form en el controlador #91

--- ProyAdoPet/ProyAdoPet/Controllers/SolicitudController.cs
+++ ProyAdoPet/ProyAdoPet/Controllers/SolicitudController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProyAdoPet.Models;
+using ProyAdoPet.Services;
 
 namespace ProyAdoPet.Controllers
 {
     public class SolicitudController : Controller
     {
-        public IActionResult Index()
+        private readonly SolicitudService _solicitudService;
+        private readonly MascotaService _mascotaService;
+
+        public SolicitudController(SolicitudService solicitudService, MascotaService mascotaService)
         {
-            return View();
+            _solicitudService = solicitudService;
+            _mascotaService = mascotaService;
         }
+
+        //HU-16: SOLICITUD DE ADOPCION
+        [HttpGet]
+        public IActionResult Postular(int mascotaId)
+        {
+            var mascota = _mascotaService.ObtenerMascota(mascotaId);
+            if (mascota == null) return NotFound();
+
+            ViewBag.MascotaNombre = mascota.Nombre;
+            ViewBag.MascotaFoto = mascota.FotoMascota;
+
+            var modelo = new SolicitudAdopcion { MascotaId = mascotaId };
+            return View(modelo);
+        }
+
+
     }
 }
---
+++ ProyAdoPet/ProyAdoPet/Services/SolicitudService.cs
@@ -0,0 +1,23 @@
+﻿using ProyAdoPet.Models;
+using ProyAdoPet.Repository;
+
+namespace ProyAdoPet.Services
+{
+    public class SolicitudService
+    {
+        ISolicitudAdopcion _solicitud;
+
+        public SolicitudService(ISolicitudAdopcion solicitud)
+        {
+            _solicitud = solicitud;
+        }
+
+        //HU-16: REGISTRO DE SOLICITUD DE ADOPCION
+        public bool EnviarSolicitud(SolicitudAdopcion solicitud)
+        {
+            if (solicitud.MascotaId <= 0 || solicitud.UsuarioId <= 0) return false;
+            return _solicitud.Registrar(solicitud);
+        }
+
+    }
+}
