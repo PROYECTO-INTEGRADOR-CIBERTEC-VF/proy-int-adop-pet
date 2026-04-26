a55e59dd1bc84dfb9cb877e93509fd236f635ae0
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-26 18:58:53 -0500

    HU-13: Definicion del viewmodel para mostrar la lista de solicitudes enviades close #95

---
+++ ProyAdoPet/ProyAdoPet/ViewModel/SolicitudAdopcionVM.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyAdoPet.ViewModel
+{
+    public class SolicitudAdopcionVM
+    {
+        public int Id { get; set; }
+
+        public string? NombrePostulante { get; set; }
+        public string? DNI { get; set; }
+
+        public string? NombreMascota { get; set; }
+        public string? FotoMascota { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public string? EstadoNombre { get; set; }
+        public int EstadoId { get; set; }
+    }
+}
