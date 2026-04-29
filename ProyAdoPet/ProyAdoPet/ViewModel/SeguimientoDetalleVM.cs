091381f4d516f7e3ae25e699955c4d6087e55c2e
Ronaldo <abarcaronaldo18@gmail.com>
2026-04-29 18:27:18 -0500

    hu-21 implementado viewmodel para el detalle seguimineto adopcion #166

---
+++ ProyAdoPet/ProyAdoPet/ViewModel/SeguimientoDetalleVM.cs
@@ -0,0 +1,11 @@
+﻿namespace ProyAdoPet.ViewModel
+{
+    public class SeguimientoDetalleVM
+    {
+        public int SolicitudId { get; set; }
+        public string? MascotaNombre { get; set; }
+        public string? FotoMascota { get; set; }
+        public string? CodigoContrato { get; set; }
+        public List<SeguimientoItemVM> Controles { get; set; } = new List<SeguimientoItemVM>();
+    }
+}
